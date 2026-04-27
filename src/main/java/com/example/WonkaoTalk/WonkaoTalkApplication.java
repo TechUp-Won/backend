a9166f4628233bb107e057518e425fbb680ffa82
HyoungukJo <whguddnr9@gmail.com>
2026-04-27 11:34:34 +0900

    feat: first commit

---
+++ src/main/java/com/example/WonkaoTalk/WonkaoTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.WonkaoTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonkaoTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonkaoTalkApplication.class, args);
+	}
+
+}
