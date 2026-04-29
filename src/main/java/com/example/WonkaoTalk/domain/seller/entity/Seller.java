4812c5c761300bbcb79c5998e33f585e108565e3
J4E-mik <wz9384@gmail.com>
2026-04-29 12:18:02 +0900

    fix: 테이블 명 수정

--- src/main/java/com/example/WonkaoTalk/domain/seller/entity/Seller.java
+++ src/main/java/com/example/WonkaoTalk/domain/seller/entity/Seller.java
@@ -20,7 +20,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "users")
+@Table(name = "sellers")
 public class Seller {
 
   @Id
--- src/main/java/com/example/WonkaoTalk/domain/seller/entity/SellerConsent.java
+++ src/main/java/com/example/WonkaoTalk/domain/seller/entity/SellerConsent.java
@@ -22,7 +22,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "users")
+@Table(name = "seller_consents")
 public class SellerConsent {
 
   @Id
