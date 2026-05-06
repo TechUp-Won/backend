0a65d7e615b26c3342b78ef0acda2454f105aace
윤수현 <ysh@knu.ac.kr>
2026-05-06 11:17:54 +0900

    refactor: 패키지 임포트문 최적화

--- src/main/java/com/example/WonkaoTalk/domain/chat/dto/ChatRoomCreateRequest.java
+++ src/main/java/com/example/WonkaoTalk/domain/chat/dto/ChatRoomCreateRequest.java
@@ -1,10 +1,11 @@
 package com.example.WonkaoTalk.domain.chat.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record ChatRoomCreateRequest(
-    @jakarta.validation.constraints.NotNull(message = "상대방 ID는 필수입니다.")
+    @NotNull(message = "상대방 ID는 필수입니다.")
     Long receiverId
 ) {
 
