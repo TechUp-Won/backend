15423365ba6002137ba6f953161b5d555151defa
HyoungukJo <whguddnr9@gmail.com>
2026-05-06 14:54:01 +0900

    refactor: ID 타입 Long 타입으로 변경

--- src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
+++ src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
@@ -19,7 +19,7 @@ public class Delivery {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
+  private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "order_id", nullable = false)
