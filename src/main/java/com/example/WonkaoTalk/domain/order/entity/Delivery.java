66ab5f2058cc6d57f1bfbb4e9d4e8826008d34bf
HyoungukJo <whguddnr9@gmail.com>
2026-05-06 15:30:21 +0900

    refactor: AuditingEntityListener 어노테이션 추가

--- src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
+++ src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
@@ -2,6 +2,7 @@ package com.example.WonkaoTalk.domain.order.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,8 +13,10 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "deliveries")
 public class Delivery {
 
