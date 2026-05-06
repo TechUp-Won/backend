77f5e094f82e05e6aec121264b8ba8a715aaba05
HyoungukJo <whguddnr9@gmail.com>
2026-05-06 15:31:20 +0900

    refactor: Enum 값을 String으로 저장하도록 수정

--- src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
+++ src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
@@ -3,6 +3,8 @@ package com.example.WonkaoTalk.domain.order.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -43,6 +45,7 @@ public class Delivery {
   @Column(nullable = false)
   private String addressDetail;
 
+  @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private DeliveryStatus deliveryStatus;
 
