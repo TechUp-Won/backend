73af19c19b2953bc08f6bcbe2c9df6dff48091ab
김민준 <minjoon9560@naver.com>
2026-05-11 22:48:57 +0900

    refactor: MiniO설정에 pathStyleAccessEnabled 설정 추가

--- src/main/java/com/example/WonkaoTalk/common/config/S3Config.java
+++ src/main/java/com/example/WonkaoTalk/common/config/S3Config.java
@@ -9,6 +9,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
@@ -47,6 +48,9 @@ public class S3Config {
         .credentialsProvider(StaticCredentialsProvider.create(
             AwsBasicCredentials.create(accessKey, secretKey)))
         .region(Region.AP_NORTHEAST_2)
+        .serviceConfiguration(S3Configuration.builder()
+            .pathStyleAccessEnabled(true)
+            .build())
         .build();
   }
 
