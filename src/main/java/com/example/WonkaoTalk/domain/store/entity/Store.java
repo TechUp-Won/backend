94ea6d9e81bd2bf2feb6e95e8d8c771ae101b358
J4E-mik <wz9384@gmail.com>
2026-05-11 16:23:56 +0900

    feat: 스토어 삭제 메소드 작성

--- src/main/java/com/example/WonkaoTalk/domain/store/entity/Store.java
+++ src/main/java/com/example/WonkaoTalk/domain/store/entity/Store.java
@@ -12,11 +12,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -27,6 +30,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE stores SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
 @Table(name = "stores")
 public class Store {
 
@@ -67,5 +72,13 @@ public class Store {
     this.description = description;
     this.phone = phone;
     this.thumbnail = thumbnail;
+  }
+
+  public void deleteStore() {
+    this.name = "삭제된 스토어" + UUID.randomUUID().toString().substring(8);
+    this.description = "DELETED";
+    this.phone = "000-0000-0000";
+    this.thumbnail = "http://defaultThumbnail.png";
+    this.deletedAt = LocalDateTime.now();
   }
 }
