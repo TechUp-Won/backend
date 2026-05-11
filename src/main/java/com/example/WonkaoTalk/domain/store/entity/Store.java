06bd9816e24e72e9703ca5cbf49a0c84bc5a3d6b
J4E-mik <wz9384@gmail.com>
2026-05-11 15:47:40 +0900

    feat: Store 도메인 엔티티 및 리포 작성

---
+++ src/main/java/com/example/WonkaoTalk/domain/store/entity/Store.java
@@ -0,0 +1,60 @@
+package com.example.WonkaoTalk.domain.store.entity;
+
+import com.example.WonkaoTalk.domain.seller.entity.Seller;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "stores")
+public class Store {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column
+  private String description;
+
+  @Column(nullable = false)
+  private String phone;
+
+  @Builder.Default
+  @Column(nullable = false)
+  private String thumbnail = "http://defaultThumbnail.png";
+
+  @OneToOne
+  @JoinColumn(name = "seller_id", nullable = false)
+  private Seller seller;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+}
