69982cffc0a6d1b995370da5335d56d11f0557e5
J4E-mik <wz9384@gmail.com>
2026-05-16 02:03:51 +0900

    test: 판매자 정보 조회/수정 단위테스트 작성

--- src/test/java/com/example/WonkaoTalk/domain/seller/service/SellerServiceTest.java
+++ src/test/java/com/example/WonkaoTalk/domain/seller/service/SellerServiceTest.java
@@ -10,16 +10,20 @@ import com.example.WonkaoTalk.domain.auth.enums.Role;
 import com.example.WonkaoTalk.domain.auth.repo.AuthRepo;
 import com.example.WonkaoTalk.domain.auth.service.AuthService;
 import com.example.WonkaoTalk.domain.seller.dto.SellerRegisterRequest;
+import com.example.WonkaoTalk.domain.seller.dto.SellerResponse;
 import com.example.WonkaoTalk.domain.seller.dto.SellerSignUpRequest;
+import com.example.WonkaoTalk.domain.seller.dto.SellerUpdateRequest;
 import com.example.WonkaoTalk.domain.seller.entity.Seller;
 import com.example.WonkaoTalk.domain.seller.repo.SellerRepo;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class SellerServiceTest {
@@ -34,6 +38,18 @@ class SellerServiceTest {
   @Mock
   private AuthService authService;
 
+  private Seller seller;
+
+  @BeforeEach
+  void setUp() {
+    seller = Seller.builder()
+        .name("침착맨스토어")
+        .phone("02-3456-7890")
+        .buzNo("1234567890")
+        .build();
+    ReflectionTestUtils.setField(seller, "id", 1L);
+  }
+
   @Test
   @DisplayName("기존 사용자의 판매자 등록 성공")
   public void registerSellerSuccess() {
@@ -41,7 +57,7 @@ class SellerServiceTest {
     Long authId = 1L;
     SellerRegisterRequest request = SellerRegisterRequest.builder()
         .buzNo("1234567890")
-        .name("Store1")
+        .name("판매자1")
         .phone("010-1234-5678")
         .build();
 
@@ -68,8 +84,9 @@ class SellerServiceTest {
     SellerSignUpRequest request = SellerSignUpRequest.builder()
         .email("test@gmail.com")
         .password("Qwer1234")
+        .passwordCheck("Qwer1234")
         .buzNo("1234567890")
-        .name("Store1")
+        .name("판매자1")
         .phone("010-1234-5678")
         .build();
 
@@ -91,4 +108,34 @@ class SellerServiceTest {
 
   }
 
+  @Test
+  @DisplayName("판매자 정보 조회 성공")
+  public void getMySellerInfoSuccess() {
+    //given
+    given(sellerRepo.findById(1L)).willReturn(Optional.of(seller));
+
+    //when
+    SellerResponse response = sellerService.getSellerInfo(1L);
+
+    //then
+    assertThat(response.sellerId()).isEqualTo(1L);
+    assertThat(response.name()).isEqualTo("침착맨스토어");
+    assertThat(response.buzNo()).isEqualTo("1234567890");
+  }
+
+  @Test
+  @DisplayName("판매자 정보 수정 성공")
+  public void updateSellerInfoSuccess() {
+    //given
+    SellerUpdateRequest request = new SellerUpdateRequest("이병건스토어", "010-1111-1111");
+    given(sellerRepo.findById(1L)).willReturn(Optional.of(seller));
+
+    //when
+    SellerResponse response = sellerService.updateSellerInfo(1L, request);
+
+    //then
+    assertThat(response.sellerId()).isEqualTo(1L);
+    assertThat(response.name()).isEqualTo("이병건스토어");
+    assertThat(response.phone()).isEqualTo("010-1111-1111");
+  }
 }
