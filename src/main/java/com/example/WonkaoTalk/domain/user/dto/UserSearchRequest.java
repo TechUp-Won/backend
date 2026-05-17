bcb23af1dbd9ccf6dd1ced4af69ae2ecf35901a3
J4E-mik <wz9384@gmail.com>
2026-05-17 21:42:16 +0900

    feat: 사용자 검색 DTO 작성

---
+++ src/main/java/com/example/WonkaoTalk/domain/user/dto/UserSearchRequest.java
@@ -0,0 +1,10 @@
+package com.example.WonkaoTalk.domain.user.dto;
+
+import jakarta.validation.constraints.Pattern;
+
+public record UserSearchRequest(
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호를 입력해주세요.")
+    String phone
+) {
+
+}
