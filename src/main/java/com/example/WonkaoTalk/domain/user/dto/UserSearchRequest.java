ee861e5c2508799bc7e40d7b0f639bf1814bb5f4
J4E-mik <wz9384@gmail.com>
2026-05-17 22:45:37 +0900

    style: 에러 메세지 수정

--- src/main/java/com/example/WonkaoTalk/domain/user/dto/UserSearchRequest.java
+++ src/main/java/com/example/WonkaoTalk/domain/user/dto/UserSearchRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record UserSearchRequest(
     @NotBlank
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호를 입력해주세요.")
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "올바른 전화번호 형식(000-0000-0000)이 아닙니다.")
     String phone
 ) {
 
