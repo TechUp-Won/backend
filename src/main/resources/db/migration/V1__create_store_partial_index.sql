34f08ee4d649f0311b4166ecdd043e7641c33f38
J4E-mik <wz9384@gmail.com>
2026-05-19 16:02:42 +0900

    fix: FLYWAY 초기 설정 수정

--- src/main/resources/db/migration/V1__create_store_partial_index.sql
+++ src/main/resources/db/migration/V1__create_store_partial_index.sql
@@ -1,5 +1,5 @@
-ALTER TABLE stores DROP CONSTRAINT uk2yl4gisselw9ehmnupuqhl3ao;
+ALTER TABLE stores DROP CONSTRAINT IF EXISTS uk2yl4gisselw9ehmnupuqhl3ao;
 
-CREATE UNIQUE INDEX uk_store_seller_active
+CREATE UNIQUE INDEX IF NOT EXISTS uk_store_seller_active
 ON stores (seller_id)
 WHERE deleted_at IS NULL;
