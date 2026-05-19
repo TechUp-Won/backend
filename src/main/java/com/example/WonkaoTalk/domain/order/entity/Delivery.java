c29dc54453aab0e3155a1cbef6370729ffb20f73
HyoungukJo <whguddnr9@gmail.com>
2026-05-19 15:46:41 +0900

    feat: Delivery 엔티티에 운송 정보 추가
    - 택배사와 운송장 번호 필드 추가
    - 배송 생성 시 초기 상태를 PREPARING으로 설정
    - 배송 상태 변경 메서드 추가

--- src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
+++ src/main/java/com/example/WonkaoTalk/domain/order/entity/Delivery.java
@@ -13,13 +13,18 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "deliveries")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Delivery {
 
   @Id
@@ -52,6 +57,13 @@ public class Delivery {
   @Column
   private String memo;
 
+  @Column
+  private String deliveryCompany;
+
+  @Column
+  private String trackingNumber; // TODO: Long으로 할지 고민중.. 운송번호 보통 다 정수일것같은데 아닐수있어서..
+
+
   @CreatedDate
   @Column(name = "created_at", nullable = false, updatable = false)
   private LocalDateTime createdAt;
@@ -59,5 +71,39 @@ public class Delivery {
   @LastModifiedDate
   @Column(name = "updated_at")
   private LocalDateTime updatedAt;
+
+  private Delivery(Order order, String recipientName, String recipientPhone, String zipcode,
+      String address, String addressDetail, String memo, DeliveryStatus deliveryStatus) {
+    this.order = order;
+    this.recipientName = recipientName;
+    this.recipientPhone = recipientPhone;
+    this.zipcode = zipcode;
+    this.address = address;
+    this.addressDetail = addressDetail;
+    this.memo = memo;
+    this.deliveryStatus = deliveryStatus;
+  }
+
+  public static Delivery createDelivery(Order order, String recipientName, String recipientPhone,
+      String zipcode,
+      String address, String addressDetail, String memo) {
+    return new Delivery(order, recipientName, recipientPhone, zipcode, address, addressDetail, memo,
+        DeliveryStatus.PREPARING);
+  }
+
+  public void markShipped(String deliveryCompany, String trackingNumber) {
+    this.deliveryCompany = deliveryCompany;
+    this.trackingNumber = trackingNumber;
+    this.deliveryStatus = DeliveryStatus.SHIPPED;
+  }
+
+  public void markInTransit() {
+    this.deliveryStatus = DeliveryStatus.IN_TRANSIT;
+  }
+
+  public void markDelivered() {
+    this.deliveryStatus = DeliveryStatus.DELIVERED;
+  }
+
 
 }
