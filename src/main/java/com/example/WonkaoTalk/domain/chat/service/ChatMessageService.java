c9c2549f1f20c844e66ed91e907a28216bd2baa8
윤수현 <ysh@knu.ac.kr>
2026-05-19 10:18:31 +0900

    refactor: 웹소켓 전송 책임 controller로 위임 및 강결합 문제 해결

--- src/main/java/com/example/WonkaoTalk/domain/chat/service/ChatMessageService.java
+++ src/main/java/com/example/WonkaoTalk/domain/chat/service/ChatMessageService.java
@@ -22,11 +22,8 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 @RequiredArgsConstructor
@@ -36,7 +33,6 @@ public class ChatMessageService {
   private final ChatMessageRepo chatMessageRepo;
   private final ChatRoomRepo chatRoomRepo;
   private final ChatParticipantRepo chatParticipantRepo;
-  private final SimpMessagingTemplate messagingTemplate;
   private final UserRepo userRepo;
 
   @Transactional
@@ -73,17 +69,7 @@ public class ChatMessageService {
         chatRoomId);
     int unreadCount = calculateUnreadCount(chatMessage.getId(), allReadMessageIds);
 
-    ChatMessageResponse response = ChatMessageResponse.of(chatMessage, me.getNickname(),
-        unreadCount);
-
-    TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
-      @Override
-      public void afterCommit() {
-        messagingTemplate.convertAndSend("/sub/chat/room/" + chatRoomId, response);
-      }
-    });
-
-    return response;
+    return ChatMessageResponse.of(chatMessage, me.getNickname(), unreadCount);
   }
 
   @Transactional
