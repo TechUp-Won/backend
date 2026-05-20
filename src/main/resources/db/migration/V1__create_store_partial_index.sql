187134f6fe9bab18f75f8b748f9b743f41bf3498
J4E-mik <wz9384@gmail.com>
2026-05-20 16:23:25 +0900

    fix: 스키마가 존재하지 않을 시 스프링 실행이 되지 않던 버그 수정

--- src/main/resources/db/migration/V1__create_store_partial_index.sql
+++ src/main/resources/db/migration/V1__create_store_partial_index.sql
@@ -1,3 +1,12 @@
+CREATE TABLE stores (
+                        id BIGSERIAL PRIMARY KEY,
+                        seller_id BIGINT NOT NULL,
+                        name VARCHAR(255) NOT NULL,
+                        deleted_at TIMESTAMP,
+                        created_at TIMESTAMP NOT NULL,
+                        updated_at TIMESTAMP NOT NULL
+);
+
 ALTER TABLE stores DROP CONSTRAINT IF EXISTS uk2yl4gisselw9ehmnupuqhl3ao;
 
 CREATE UNIQUE INDEX IF NOT EXISTS uk_store_seller_active
