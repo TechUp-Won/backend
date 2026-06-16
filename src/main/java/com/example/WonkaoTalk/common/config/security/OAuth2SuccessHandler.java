2bab9435a6b72b9de7d333d2247d4b72a4b04079
J4E-mik <wz9384@gmail.com>
2026-06-16 16:47:10 +0900

    style: url 생성 방식 변경

--- src/main/java/com/example/WonkaoTalk/common/config/security/OAuth2SuccessHandler.java
+++ src/main/java/com/example/WonkaoTalk/common/config/security/OAuth2SuccessHandler.java
@@ -58,6 +58,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     response.addCookie(createCookie("refresh-token", refreshToken, true,
         (int) (jwtTokenProvider.getRefreshTokenValidTime() / 1000)));
+    // response.addCookie(createCookie("access-token", accessToken, false, 60));
 
     OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
     OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
@@ -72,8 +73,12 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
       });
     }
 
-    getRedirectStrategy().sendRedirect(request, response,
-        frontEndProperties.oauthRedirectUri() + "?accessToken=" + accessToken);
+    String targetUrl = org.springframework.web.util.UriComponentsBuilder
+        .fromUriString(frontEndProperties.oauthRedirectUri())
+        .queryParam("accessToken", accessToken)
+        .build().toUriString();
+
+    getRedirectStrategy().sendRedirect(request, response, targetUrl);
   }
 
   private Cookie createCookie(String name, String value, boolean httpOnly, int maxAge) {
